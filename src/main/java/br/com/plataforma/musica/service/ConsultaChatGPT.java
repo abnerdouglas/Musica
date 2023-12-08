6d7cde6247800174b2e631ccd3f3d37756c8babe
Abner <abner.rmachado@gmail.com>
2023-12-07 23:29:34 -0300

    apaga apikey

--- src/main/java/br/com/plataforma/musica/service/ConsultaChatGPT.java
+++ src/main/java/br/com/plataforma/musica/service/ConsultaChatGPT.java
@@ -6,7 +6,7 @@ import com.theokanning.openai.service.OpenAiService;
 public class ConsultaChatGPT {
 
     public static String pesquisarArtista(String texto) {
-        OpenAiService service = new OpenAiService(System.getenv("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"));
+        OpenAiService service = new OpenAiService(System.getenv("OPENAI_APIKEY"));
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("text-davinci-003")
